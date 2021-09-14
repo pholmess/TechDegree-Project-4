8011e92ef3faee4c618712db857f83dcddf8b500
Sherri Holmes <85898920+pholmess@users.noreply.github.com>
2021-09-14 15:50:02 -0400

    changed directory

---
+++ js/array.js
@@ -0,0 +1,10 @@
+createPhrases(){
+    const phraseArray = [
+          new Phrase('The sky is the limit'),
+          new Phrase('Piece of cake'),
+          new Phrase('Break a leg'),
+          new Phrase('Under the weather'),
+          new Phrase('Down to earth')
+          ]
+    return phraseArray
+}; 
